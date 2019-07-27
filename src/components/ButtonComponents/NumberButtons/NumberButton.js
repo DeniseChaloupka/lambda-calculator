665febcdc093d7750e708dd8f465d258911c583c
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-07-26 22:32:48 -0500

    updated numbers

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,43 @@
 import React from "react";
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+const NumberButton = ({ number, total }) => { // Passing {number} as props
+  let style_NumberButton;
+  // eslint-disable-next-line
+  if (number == 0) {
+    style_NumberButton = {
+      background: '#0E447E',
+      width: '180px',
+      height:'75px',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '40px',
+      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.20)',
+      cursor: 'pointer',
+      outline: 'none',
+    }
+  } else {
+    style_NumberButton = {
+      background: '#0E447E',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '40px',
+      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.20)',
+      cursor: 'pointer',
+      outline: 'none',
+    }
+  }
+
+  return ( 
+    <> {/* Display a button element rendering the data being passed down from the parent container on props */ } 
+    <button 
+      className = 'clicky'
+      style = { style_NumberButton }
+      onClick = { total }> 
+      { number } 
+      </button> 
+      </>
   );
 };
+
+// Exporting the {NumberButton} component using the `export` keyword
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { numbers } from '../../../data.js';
+import NumberButton from './NumberButton';
 
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Numbers = () => {
-  // STEP 2 - add the imported data to state
+const Numbers = ({ total }) => {
+    // STEP 2 - add the imported data to state
+    const [numbersState] = useState(numbers);
+    const style_Numbers = {
+      display: 'flex',
+      flexWrap: 'wrap',
+      width: '300px',
+      justifyContent: 'space-around',
+      height: '400px',
+      order: '3',
+    }
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+    < div style = { style_Numbers } > 
+    {/* STEP 3 - Use .map() to iterate over your array data and return a button
+     component matching the name on the provided file. Pass
+     it any props needed by the child component*/ } 
+     {numbersState.map((number, i) => {
+        return <NumberButton key = { i } number = { number } />
+      })}
     </div>
   );
 };
+
+export default Numbers;
