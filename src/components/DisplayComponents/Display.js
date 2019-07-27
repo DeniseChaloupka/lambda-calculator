81b47bc76462249ae91328c956cbee850205bcd9
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-07-26 22:33:24 -0500

    updated display

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-const Display = () => {
-  return <div>{/* Display any props data here */}</div>;
+const Display = ({total}) => {
+  const style_Display = {
+    margin: 'auto',
+    display: 'block',
+    background: '#24292e',
+    width: '375px',
+    height: '90px',
+    borderRadius: '50px',
+    color: '#fff',
+    fontSize: '55px',
+    textAlign: 'center',
+    padding: '5px',
+  }
+
+  return <div style = { style_Display} > { total } </div>;
 };
+
+// Exporting the {Display} component using the `export` keyword
+export default Display;
