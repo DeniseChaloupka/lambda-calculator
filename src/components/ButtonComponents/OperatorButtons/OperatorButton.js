fc158c01e45b36c7e63c42f935ba489ec03ed358
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-07-26 22:33:00 -0500

    updated operators

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-const OperatorButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
+const OperatorButton = ({ operator, value, total }) => { // Passing {operator, value} as props
+  const style_Operator = {
+    background: '#007497',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '40px',
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.20)',
+    cursor: 'pointer',
+    outline: 'none',
+  }
+
+  return ( <> 
+    {/* Display a button element rendering the data being passed down from the parent container on props */ } 
+    <button className = 'clicky'
+    style = { style_Operator }
+    onClick = { total } value = { value }> 
+    { operator } 
+    </button> 
     </>
   );
 };
+
+// Exporting the {OperatorButton} component using the `export` keyword
+export default OperatorButton;
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,16 +1,39 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { operators } from '../../../data.js';
+import OperatorButton from './OperatorButton';
 //import any components needed
 
 //Import your array data to from the provided data file
 
-const Operators = () => {
+
+const Operators = ({ total }) => {
   // STEP 2 - add the imported data to state
-  return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+  const [operatorsState] = useState(operators);
+
+  const style_Operators = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    marginBottom: '13px',
+    height: '100%',
+    order: '3'
+  }
+
+  return ( 
+    <div style = { style_Operators }> 
+    {/* STEP 3 - Use .map() to iterate over your array data and return a button
+     component matching the name on the provided file. Pass
+     it any props needed by the child component*/ } 
+     {operatorsState.map(operator => {
+        return <OperatorButton
+        operator = { operator.char }
+        value = { operator.value }
+        total = { total } />
+      })
+    } 
     </div>
   );
 };
+
+// Exporting the {Operators} component using the `export` keyword
+export default Operators;
