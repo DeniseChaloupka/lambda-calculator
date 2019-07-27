20248199bf403e43e1a1fecc8a362db647be63f6
Denise Chaloupka <denisechaloupka@yahoo.com>
2019-07-26 22:33:34 -0500

    updated logo

--- src/components/DisplayComponents/Logo.js
+++ src/components/DisplayComponents/Logo.js
@@ -2,10 +2,11 @@ import React from "react";
 
 import logo from "../../Img/Lambda_Logo_white.png";
 
-const Logo = () => {
-  return (
-    <div className="logo-container">
-      <img className="logo" src={logo} alt="Lambda's logo" />
+const Logo = ({style}) => {
+  return ( 
+  <div className = "logo-container"
+    style = { style } >
+    <img className = "logo" src = { logo } alt = "Lambda's logo" />
     </div>
   );
 };
